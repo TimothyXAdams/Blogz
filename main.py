b2e2b51938fd873f0657514cc460f6cd8aa6125d
Timothy Adams <TimothyXAdams@gmail.com>
2017-07-16 20:41:15 -0700

    Project complete.

--- main.py
+++ main.py
@@ -21,12 +21,12 @@ class BlogEntry(db.Model):
         self.entry = entry
 
     def is_valid(self):
-        if self.title != '' and self.entry != '':
+        if self.title and self.entry :
             return True
         else:
             return False
 
-@app.route('/add_a_new_post', methods=['POST'])
+@app.route('/add_a_new_post', methods=['POST', 'GET'])
 def add_a_new_post():
 
         if request.method == 'POST':
@@ -37,16 +37,15 @@ def add_a_new_post():
             if new_post.is_valid() == True:
                 db.session.add(new_post)
                 db.session.commit()
-                url = "/entry?id=" + str(new_post.id)
+                url = "/blog?id=" + str(new_post.id)
                 return redirect(url)
             else: #if the validation came up False
                 flash("Error. Your blog entry requires a title and a text.")
-                return render_template('add_a_new_post', title="Enter your blog here.", entry_title=entry_title, entry_text=entry_text)
+                return render_template('add_a_new_post.html', title="Enter your blog here.", entry_title=entry_title, entry_text=entry_text)
 
         else: #it was a GET request, not a POST
-            return render_template('add_a_new_post')
+            return render_template('add_a_new_post.html',title="Add a New Post")
 
-        return(render_template('add_a_new_post.html',title="Add a New Post"))
 
 
 
@@ -62,19 +61,23 @@ def show_blogs():
     blog_id = request.args.get('id')
     if blog_id: #if the blog isn't empty
         blog = BlogEntry.query.get(blog_id)
+        if blog:
         # return str(blog.title) + str(blog.entry)
-        return render_template('blog.html', title=blog.title, blog=blog)
-    else:
-        blogs = BlogEntry.query.all()
-        return render_template('all_blogs.html', title="entry_title", blogs=blogs) #completed_tasks=completed_tasks)
+            return render_template('blog.html', title=blog.title, blog=blog)
+        else:
+            flash("Invalid Blog ID")
+
+    blogs = BlogEntry.query.all()
+    return render_template('all_blogs.html', title="entry_title", blogs=blogs) #completed_tasks=completed_tasks)
 
 
 @app.route("/updatedb")
 def update_DB():
+
     db.drop_all()
     db.create_all()
-    return "updated db"
-    #return redirect("/")
+    #return "updated db"
+    return redirect("/")
 
 
 if __name__ == '__main__':
